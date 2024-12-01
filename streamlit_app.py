737c3ada66ebb9c3b36f894013d1ac5b05bf52cb
Nya Feinstein <110700248+nyafein@users.noreply.github.com>
2024-12-01 06:04:07 +0000

    first

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,38 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Title and headers
+st.markdown("<h1 style='text-align: center;'>Syntax, Semantics, and Society</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>🌐</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>Machine Learning Based Analysis of Linguistic Relativity</h3>", unsafe_allow_html=True)
+
+# Section: Data and Methods
+st.markdown("<h4 style='text-align: center; text-decoration: underline;'>Data and Methods</h4>", unsafe_allow_html=True)
+
+# Dataset section
+st.markdown("**Dataset:**")
+st.markdown("[MAKED: Multi-lingual Automatic Keyword Extraction Dataset](https://aclanthology.org/)")
+
+# Transformers section
+st.markdown("**Transformers:**")
+st.markdown("[DistilBERT Documentation](https://huggingface.co/distilbert)")
+st.markdown("[Explanation of BERT Model](https://www.geeksforgeeks.org/explanation-of-bert-model-nlp/)")
+st.markdown("[Guide to Positional Embeddings](https://harrisonpim.com/blog/understanding-positional-embeddings-in-transformer-models)")
+
+# Section: Linguistics
+st.markdown("<h4 style='text-align: center; text-decoration: underline;'>Linguistics</h4>", unsafe_allow_html=True)
+
+# Linguistic Theory section
+st.markdown("**Linguistic Theory:**")
+st.markdown("[Sapir-Whorf Hypothesis (I)](https://www.verywellmind.com/sapir-whorf-hypothesis-3024367)")
+st.markdown("[Sapir-Whorf Hypothesis (II)](https://plato.stanford.edu/entries/linguistic-relativity/)")
+
+# Video Resources section
+st.markdown("**Video Resources:**")
+st.markdown("[Linguistic Relativity: How Language Shapes Thought](https://www.youtube.com/watch?v=5J2vn-wjGVk)")
+st.markdown("[Do we think differently in different languages?](https://www.youtube.com/watch?v=cJqZaHE3hXQ)")
+
+# Language Families section
+st.markdown("**Language Families:**")
+st.markdown("[Map of The Primary Human Language Families](https://www.brilliantmaps.com/language-families/)")
+st.markdown("[Index of Languages by Language Family](https://en.wikipedia.org/wiki/List_of_language_families)")
+st.markdown("[Language Families of the World](https://www.thoughtco.com/language-families-4074531)")
