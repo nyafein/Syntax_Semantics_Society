db1f69685a50e6210c34d15a3b90ac6afeafe1f3
Nya Feinstein <110700248+nyafein@users.noreply.github.com>
2024-12-04 17:49:13 +0000

    tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,62 @@
 import streamlit as st
 
-# Title and headers
-st.markdown("<h1 style='text-align: center;'>Syntax, Semantics, and Society</h1>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center;'>🌐</h1>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center;'>Machine Learning Based Analysis of Linguistic Relativity</h3>", unsafe_allow_html=True)
+tab1, tab2 = st.tabs(["Sources", "Appendix"])
 
-# Section: Data and Methods
-st.markdown("<h4 style='text-align: center; text-decoration: underline;'>Data and Methods</h4>", unsafe_allow_html=True)
+with tab1:
 
-# Dataset section
-st.markdown("**Dataset:**")
-st.markdown("[MAKED: Multi-lingual Automatic Keyword Extraction Dataset](https://aclanthology.org/2022.lrec-1.664/)")
+    # Title and headers
+    st.markdown("<h1 style='text-align: center;'>Syntax, Semantics, and Society</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>🌐</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'>Machine Learning Based Analysis of Linguistic Relativity</h3>", unsafe_allow_html=True)
 
-# Transformers section
-st.markdown("**Transformers:**")
-st.markdown("[DistilBERT Documentation](https://huggingface.co/docs/transformers/model_doc/distilbert)")
-st.markdown("[Explanation of BERT Model](https://www.geeksforgeeks.org/explanation-of-bert-model-nlp/)")
-st.markdown("[Guide to Positional Embeddings](https://harrisonpim.com/blog/understanding-positional-embeddings-in-transformer-models)")
+    # Section: Data and Methods
+    st.markdown("<h4 style='text-align: center; text-decoration: underline;'>Data and Methods</h4>", unsafe_allow_html=True)
 
-# Section: Linguistics
-st.markdown("<h4 style='text-align: center; text-decoration: underline;'>Linguistics</h4>", unsafe_allow_html=True)
+    # Dataset section
+    st.markdown("**Dataset:**")
+    st.markdown("[MAKED: Multi-lingual Automatic Keyword Extraction Dataset](https://aclanthology.org/2022.lrec-1.664/)")
 
-# Linguistic Theory section
-st.markdown("**Linguistic Theory:**")
-st.markdown("[Sapir-Whorf Hypothesis (I)](https://helpfulprofessor.com/sapir-whorf-hypothesis-examples/)")
-st.markdown("[Sapir-Whorf Hypothesis (II)](https://plato.stanford.edu/entries/linguistics/whorfianism.html)")
+    # Transformers section
+    st.markdown("**Transformers:**")
+    st.markdown("[DistilBERT Documentation](https://huggingface.co/docs/transformers/model_doc/distilbert)")
+    st.markdown("[Explanation of BERT Model](https://www.geeksforgeeks.org/explanation-of-bert-model-nlp/)")
+    st.markdown("[Guide to Positional Embeddings](https://harrisonpim.com/blog/understanding-positional-embeddings-in-transformer-models)")
 
-# Video Resources section
-st.markdown("**Video Resources:**")
-st.markdown("[Linguistic Relativity: How Language Shapes Thought](https://www.youtube.com/watch?v=Q7yyYdiNTIo)")
-st.markdown("[Do we think differently in different languages?](https://www.youtube.com/watch?v=XINQvKbqzq0)")
+    # Section: Linguistics
+    st.markdown("<h4 style='text-align: center; text-decoration: underline;'>Linguistics</h4>", unsafe_allow_html=True)
 
-# Language Families section
-st.markdown("**Language Families:**")
-st.markdown("[Map of The Primary Human Language Families](https://brilliantmaps.com/human-language-families/)")
-st.markdown("[Index of Languages by Language Family](https://www.omniglot.com/writing/langfam.htm)")
-st.markdown("[Language Families of the World](https://testyourlanguage.com/blog/language-families-of-the-world)")
+    # Linguistic Theory section
+    st.markdown("**Linguistic Theory:**")
+    st.markdown("[Sapir-Whorf Hypothesis (I)](https://helpfulprofessor.com/sapir-whorf-hypothesis-examples/)")
+    st.markdown("[Sapir-Whorf Hypothesis (II)](https://plato.stanford.edu/entries/linguistics/whorfianism.html)")
+
+    # Video Resources section
+    st.markdown("**Video Resources:**")
+    st.markdown("[Linguistic Relativity: How Language Shapes Thought](https://www.youtube.com/watch?v=Q7yyYdiNTIo)")
+    st.markdown("[Do we think differently in different languages?](https://www.youtube.com/watch?v=XINQvKbqzq0)")
+
+    # Language Families section
+    st.markdown("**Language Families:**")
+    st.markdown("[Map of The Primary Human Language Families](https://brilliantmaps.com/human-language-families/)")
+    st.markdown("[Index of Languages by Language Family](https://www.omniglot.com/writing/langfam.htm)")
+    st.markdown("[Language Families of the World](https://testyourlanguage.com/blog/language-families-of-the-world)")
 
 
-st.markdown("<h4 style='text-align: center; text-decoration: underline;'>More Informtation</h4>", unsafe_allow_html=True)
-st.markdown("[GitHub Repository](https://github.com/nyafein/Syntax_Semantics_Society)")
-st.markdown("[My Contact](mailto:nyafein@gmail.com)")
+    st.markdown("<h4 style='text-align: center; text-decoration: underline;'>More Informtation</h4>", unsafe_allow_html=True)
+    st.markdown("[GitHub Repository](https://github.com/nyafein/Syntax_Semantics_Society)")
+    st.markdown("[My Contact](mailto:nyafein@gmail.com)")
 
+with tab2:
 
+    st.markdown("<h1 style='text-align: center;'>Appendix</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>📖</h1>", unsafe_allow_html=True)
+    
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.write(' ')
+    with col2:
+        st.image("./sentence_similarity.png")
+    with col3:
+        st.write(' ')
+        st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
